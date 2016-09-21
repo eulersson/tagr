c48fcde36a2d53e35ae1f92d9c911ba96e248646
Ramon Blanquer <blanquer.ramon@gmail.com>
2016-09-21 21:48:34 +0100

    Remove hard coded port for heroku

--- server/config/config.js
+++ server/config/config.js
@@ -5,12 +5,12 @@ module.exports = {
     alias: 'development',
     rootPath: path.normalize(__dirname + '/../..'),
     db: 'mongodb://localhost/tagr',
-    port: process.env.port || 3030
+    port: process.env.PORT || 3030
   },
   production: {
     alias: 'production',
     rootPath: path.normalize(__dirname + '/../..'),
     db: 'mongodb://rblanquer:eulersson@ds063725.mlab.com:63725/heroku_3b1zpcq7',
-    port: process.env.port || 80
+    port: process.env.PORT || 80
   }
 }
